e4f964c276280c801db822280796e211b042acde
Peter <pitrens.studio@gmail.com>
2019-08-06 21:01:33 +0800

    add badge.dart

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -4,15 +4,26 @@ import 'package:provider/provider.dart';
 import './pages/products_details.dart';
 import './pages/products_overview.dart';
 import './provider/products.dart';
+import './provider/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(), // the provider class / myNotifier
-      child: MaterialApp( // widgets that might listened to products change
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        )
+      ],
+
+      // the provider class / myNotifier
+      child: MaterialApp(
+        // widgets that might listened to products change
         title: 'MyShop',
         theme: ThemeData(
           primarySwatch: Colors.purple,
--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -3,15 +3,17 @@ import 'package:provider/provider.dart';
 
 import '../pages/products_details.dart';
 import '../provider/product.dart';
+import '../provider/cart.dart';
 
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-    return buildProductview(context, product);
+    final cart = Provider.of<Cart>(context, listen: false);
+    return buildProductview(context, product , cart);
   }
 
-  Widget buildProductview(BuildContext context, Product product) {
+  Widget buildProductview(BuildContext context, Product product, cart) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -45,7 +47,9 @@ class ProductItem extends StatelessWidget {
             trailing: IconButton(
               icon: Icon(Icons.shopping_cart),
               color: Theme.of(context).accentColor,
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product.id, product.price, product.title);
+              },
             ),
           ),
         ),
