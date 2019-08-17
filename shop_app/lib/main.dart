1f67b885a8331688a8e4a509b574b1b6dd3a5d72
Peter <pitrens.studio@gmail.com>
2019-08-18 00:03:58 +0800

    restarting shop app

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import './pages/products_details.dart';
-import './pages/products_overview.dart';
-import './provider/products.dart';
-import './provider/cart.dart';
+import './screens/products_overview.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: Products(),
-        ),
-        ChangeNotifierProvider.value(
-          value: Cart(),
-        )
-      ],
-
-      // the provider class / myNotifier
-      child: MaterialApp(
-        // widgets that might listened to products change
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: "Lato",
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        },
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
       ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -1,56 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../pages/products_details.dart';
-import '../provider/product.dart';
-import '../provider/cart.dart';
+import './../screens/product_detail.dart';
 
 class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context, listen: false);
-    final cart = Provider.of<Cart>(context, listen: false);
-    return buildProductview(context, product , cart);
-  }
-
-  Widget buildProductview(BuildContext context, Product product, cart) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                arguments: product.id);
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title)));
           },
           child: Image.network(
-            product.imageUrl,
+            imageUrl,
             fit: BoxFit.cover,
           ),
         ),
-        footer: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: Consumer<Product>(
-              builder: (ctx, product, _) => IconButton(
-                icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
-                ),
-                color: Theme.of(context).accentColor,
-                onPressed: () => product.toggleFavoriteStatus(),
-              ),
-            ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              color: Theme.of(context).accentColor,
-              onPressed: () {
-                cart.addItem(product.id, product.price, product.title);
-              },
-            ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
           ),
         ),
       ),
